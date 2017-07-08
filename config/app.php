4ee486ada154a22e74c80eaca6ad8e5b49468d82
mymnsok <ma051778@gmail.com>
2017-07-08 21:26:57 +0000

    user pages

--- config/app.php
+++ config/app.php
@@ -196,9 +196,12 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View'      => Illuminate\Support\Facades\View::class,
         
-        // 追加
+        // Form
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
+        
+        //Gravatar
+        Thomaswelton\LaravelGravatar\LaravelGravatarServiceProvider::class,
 
     ],
 
